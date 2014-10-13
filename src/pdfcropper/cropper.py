b355e48e6d94311db8ed2dc8d3cee101df8f5066
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-13 14:03:26 +0200

    #6: Added crop_page(). crop() renamed to crop_all().

--- src/pdfcropper/cropper.py
+++ src/pdfcropper/cropper.py
@@ -13,31 +13,40 @@ from pyPdf import PdfFileWriter, PdfFileReader
 
 
 # Functions & objects =========================================================
-def crop(pdf, left, right, top, bottom):
+def crop_page(page, left, right, top, bottom):
+    page.mediaBox.upperRight = (
+        page.mediaBox.getUpperRight_x() - right,
+        page.mediaBox.getUpperRight_y() - top
+    )
+    page.mediaBox.lowerLeft = (
+        page.mediaBox.getLowerLeft_x() + left,
+        page.mediaBox.getLowerLeft_y() + bottom
+    )
+
+    return page
+
+
+def crop_all(pdf, left, right, top, bottom):
     out = PdfFileWriter()
 
     # crop pages
     for page in pdf.pages:
-        page.mediaBox.upperRight = (
-            page.mediaBox.getUpperRight_x() - right,
-            page.mediaBox.getUpperRight_y() - top
+        out.append(
+            crop_page(page, left, right, top, bottom)
         )
-        page.mediaBox.lowerLeft = (
-            page.mediaBox.getLowerLeft_x() + left,
-            page.mediaBox.getLowerLeft_y() + bottom
-        )
-        out.addPage(page)
 
     return out
 
 
 # Main program ================================================================
 if __name__ == '__main__':
-    pdf = PdfFileReader(
-        open("lpm.pdf", 'rb')
-    )
-    out_pdf = crop(pdf, 50, 50, 50, 50)
 
-    out_file = file("lpm_out.pdf", 'wb')
-    out_pdf.write(out_file)
-    out_file.close()
+
+    # pdf = PdfFileReader(
+    #     open("lpm.pdf", 'rb')
+    # )
+    # out_pdf = crop(pdf, 50, 50, 50, 50)
+
+    # out_file = file("lpm_out.pdf", 'wb')
+    # out_pdf.write(out_file)
+    # out_file.close()
