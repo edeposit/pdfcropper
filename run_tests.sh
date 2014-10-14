1a58b2619aeb6636bb72fc0f0573a98f594586f9
Bystroushaak <bystrousak@kitakitsune.org>
2014-10-14 16:08:33 +0200

    #4: run_tests.sh changed - removed integration tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,63 +1,6 @@
 #! /usr/bin/env bash
 
 PYTHONPATH="$PYTHONPATH:src/"
-TEST_PATH="src/tests"
+TEST_PATH="src/pdfcropper/unittests/"
 
-function show_help {
-    echo -e "Usage: $0 [-h] [-a] [-i] [-u]"
-    echo
-    echo -e "\t-h"
-    echo -e "\t\tShow this help."
-    echo -e "\t-a"
-    echo -e "\t\tRun all tests."
-    echo -e "\t-i"
-    echo -e "\t\tRun integration test."
-    echo -e "\t-u"
-    echo -e "\t\tRun unittest."
-    echo
-    exit;
-}
-
-function run_all_tests {
-    py.test $TEST_PATH;
-    exit
-}
-
-function run_int_tests {
-    py.test "$TEST_PATH/integration";
-    exit
-}
-
-function run_unit_tests {
-    py.test "$TEST_PATH/unittests";
-    exit
-}
-
-while getopts "haiu" optname; do
-    case "$optname" in
-        "a")
-            run_all_tests;
-        ;;
-        "i")
-            run_int_tests;
-        ;;
-        "u")
-            run_unit_tests;
-        ;;
-        "h")
-            show_help;
-        ;;
-        "?")
-            echo "Unknown option $OPTARG"
-        ;;
-        ":")
-            echo "No argument value for option $OPTARG"
-        ;;
-        *)
-            # Should not occur
-            echo "Unknown error while processing options"
-        ;;
-    esac
-done
-
-show_help;
+py.test "$TEST_PATH"
